972354848d14eedbcd65a433eb1c99c9f939b7bc
flowerburied <2842511561@qq.com>
2020-12-08 10:16:35 +0800

    配置登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,27 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+
+const routes = [{
+        path: "/",
+        name: "Login",
+        component: () =>
+            import ("../views/account/Login.vue")
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue")
+    }
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(), //带#号
+    // createWebHistory  //不带#号
+    routes
 });
 
 export default router;
