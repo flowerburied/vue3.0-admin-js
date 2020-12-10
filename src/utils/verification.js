f418709a8127ccb277dc03303f71f7f854b78982
flowerburied <2842511561@qq.com>
2020-12-10 16:49:54 +0800

    17完成

--- src/utils/verification.js
+++ src/utils/verification.js
@@ -4,6 +4,24 @@
  */
 
 export function checkPhone(value) {
-    let regPhong = /^1[369]\d{9}$/;
+    let regPhong = /^1[3456789]\d{9}$/;
     return regPhong.test(value)
+}
+
+/*
+6 - 20数字加英文
+*/
+
+
+export function checkPassword(value) {
+    let regPassword = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/;
+    return regPassword.test(value)
+}
+/*
+6 位数字加英文
+*/
+
+export function checkCode(value) {
+    let regCode = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6}$/;
+    return regCode.test(value)
 }
